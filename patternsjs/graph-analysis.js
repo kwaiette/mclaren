a25382a2972903698afe44fe721033585087d2de
kwaiette <jensenw@gmail.com>
2021-05-18 12:23:07 -0600

    auto commit generated by git-auto-update

---
+++ patternsjs/graph-analysis.js
@@ -0,0 +1,7 @@
+//types of graph analysis
+    //file is a graph
+    //file is being treated like a graph
+        //what do we consider a node? an edge?
+//what do we want to know from the graph analysis?
+    //which unconnected nodes are related?
+    
